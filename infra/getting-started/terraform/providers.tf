bc4318d17fc2c318cabb6ec46bf31e29d044bbf0
Almir Banjanovic <abanjanovic@microsoft.com>
2026-02-26 20:53:41 -0500

    Add getting-started providers.tf

---
+++ infra/getting-started/terraform/providers.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_version = ">= 1.14.6"
+
+  required_providers {
+
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.62.0"
+    }
+
+    azapi = {
+      source  = "azure/azapi"
+      version = "~> 2.8.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 3.0.1"
+    }
+
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.19.0"
+    }
+  }
+
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_deleted_certificates_on_destroy = true
+      recover_soft_deleted_certificates          = true
+      purge_soft_deleted_secrets_on_destroy      = true
+      recover_soft_deleted_secrets               = true
+    }
+  }
+
+  resource_provider_registrations = "none"
+}
+
+provider "azapi" {
+}
