16a65bf98f66e5802ed621f5dc2a30cca42ab25b
Almir Banjanovic <abanjanovic@microsoft.com>
2026-03-03 15:41:51 -0500

    Add outputs.tf

---
+++ infra/terraform/outputs.tf
@@ -0,0 +1,10 @@
+output "openai_endpoint" {
+  description = "Azure OpenAI endpoint URL for SDK usage"
+  value       = "https://${azurerm_cognitive_account.this.custom_subdomain_name}.openai.azure.com/"
+}
+
+output "openai_api_key" {
+  description = "Azure OpenAI API key"
+  value       = azurerm_cognitive_account.this.primary_access_key
+  sensitive   = true
+}
